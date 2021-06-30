4b3d0164a12a32038cc12f10d1c105d90c6334f2
jeshrz <jeshrz@foxmail.com>
2021-06-30 15:42:52 +0800

    add nvim config

---
+++ nvim/plugin/treesitter.lua
@@ -0,0 +1,16 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {"c", "cpp", "rust", "bash", "lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = {
+        "javascript", "beancount", "bibtex", "c_sharp", "clojure", "comment",
+        "commonlisp", "cuda", "dart", "devicetree", "elixir", "erlang",
+        "fennel", "Godot", "glimmer", "graphql", "java", "jsdoc", "julia",
+        "kotlin", "ledger", "nix", "ocaml", "ocaml_interface", "php", "ql",
+        "query", "r", "rst", "ruby", "scss", "sparql", "supercollider",
+        "svelte", "teal", "tsx", "typescript", "turtle", "verilog", "vue", "zig",
+		"go", "gomod"
+    }, -- List of parsers to ignore installing
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = {} -- list of language that will be disabled
+    }
+}
