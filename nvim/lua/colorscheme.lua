5450170aecf6d4778f543e0f751841336c30c1d5
jeshrz <jeshrz@foxmail.com>
2022-05-23 12:46:44 +0800

    replace dark theme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -6,7 +6,7 @@ if not status then
 end
 
 theme.setup({
-  theme_style = "light",
+  --theme_style = "light",
   function_style = "italic",
   sidebars = {"qf", "vista_kind", "terminal", "packer"},
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
