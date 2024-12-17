9fc2b00bc507f8af65a21ae4f7bbc248f3ecc8fd
jeshrz <jeshrz@foxmail.com>
2024-12-17 11:59:13 +0800

    update zshconfig proxy

--- zsh/dot-config/omz-custom/20-proxy.zsh
+++ zsh/dot-config/omz-custom/20-proxy.zsh
@@ -1,7 +1,8 @@
 function proxyon() {
-	export ALL_PROXY="http://127.0.0.1:20171"
-	export HTTP_PROXY="http://127.0.0.1:20171"
-	export HTTPS_PROXY="http://127.0.0.1:20171"
+  PORT=20171
+	export ALL_PROXY="http://127.0.0.1:${PORT}"
+	export HTTP_PROXY="http://127.0.0.1:${PORT}"
+	export HTTPS_PROXY="http://127.0.0.1:${PORT}"
 	echo "ALL_PROXY=$ALL_PROXY;HTTP_PROXY=$HTTP_PROXY;HTTPS_PROXY=$HTTPS_PROXY"
 }
 
