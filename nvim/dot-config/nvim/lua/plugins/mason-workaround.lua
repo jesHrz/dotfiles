888d0df64fe434c233d6ff42173bd97138be8c74
jeshrz <jeshrz@foxmail.com>
2025-05-19 16:37:52 +0800

    remove mason-workaround; format 11-proxy

--- nvim/dot-config/nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- mason reference updates and existing configration refers to the old reference
-  -- temporarily pins mason at v1
-  { "mason-org/mason.nvim", version = "^2.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
--- zsh/dot-config/omz-custom/11-proxy.zsh
+++ zsh/dot-config/omz-custom/11-proxy.zsh
@@ -1,12 +1,12 @@
 function proxyon() {
-  PORT=20171
-	export ALL_PROXY="http://127.0.0.1:${PORT}"
-	export HTTP_PROXY="http://127.0.0.1:${PORT}"
-	export HTTPS_PROXY="http://127.0.0.1:${PORT}"
-	echo "ALL_PROXY=$ALL_PROXY;HTTP_PROXY=$HTTP_PROXY;HTTPS_PROXY=$HTTPS_PROXY"
+  local port=20171
+  local server="http://127.0.0.1:${port}"
+
+  export ALL_PROXY="${server}" HTTPS_PROXY="${server}" HTTP_PROXY="${server}" NO_PROXY="localhost,127.0.0.1"
+	echo "ALL_PROXY=${ALL_PROXY};HTTP_PROXY=${HTTP_PROXY};HTTPS_PROXY=${HTTPS_PROXY};NO_PROXY=${NO_PROXY}"
 }
 
 function proxyoff() {
-	unset ALL_PROXY HTTP_PROXY HTTPS_PROXY
-	echo "proxyoff"
+  unset ALL_PROXY HTTP_PROXY HTTPS_PROXY NO_PROXY
+  echo "proxyoff"
 }
