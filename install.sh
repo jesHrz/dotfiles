56e6abdc129fad645adc4b769bb1ce29d4a55b05
jeshrz <jeshrz@foxmail.com>
2026-04-20 09:30:07 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-list=(zsh git nvim)
-for i in ${list[*]}; do
-  stow -Rnv -t $HOME $i --dotfiles || exit -1
-done
+app=${1}
+if [[ "x${app}" == "x" ]]; then
+	echo "No app name specified"
+	exit -1
+fi
+
+stow -Rv -t $HOME $app --dotfiles
+
+# list=(zsh git nvim)
+# for i in ${list[*]}; do
+#   stow -Rnv -t $HOME $i --dotfiles || exit -1
+# done
